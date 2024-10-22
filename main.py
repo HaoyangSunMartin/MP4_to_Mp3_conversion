4ae25b9e81d44c00990081f1da658ab5e6342fd1
Haoyang Sun <sunhaoyangmartin@gmail.com>
2024-10-22 22:32:29 +0200

    added -M option, which modifies the volume of the mp3 video by n-fold

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ the rest of the work.
 # Import the main package
 import sample_package
 from moviepy.editor import *
+
+from moviepy.audio.fx.volumex import volumex
+from moviepy.editor import AudioFileClip
 import os
 import argparse
 
 
 def parse_file_names(absolute_path:str):
     return [f for f in os.listdir(absolute_path) if (f.endswith(".mp4") and not f.startswith("._")) ]
+# def double_volume(clip:AudioClip):
+    
+#     return 
 
-def convert(root:str,files:list[str],destination:str):
+def convert(root:str,files:list[str],destination:str, volume_multiply:int):
     for index, f in enumerate(files):
         video = VideoFileClip(os.path.join(root,f))
         name = f[:-4]+".mp3"
-        video.audio.write_audiofile(os.path.join(destination,name))
+        audio = video.audio
+        
+        if volume_multiply!=1:
+            audio = volumex(audio,volume_multiply)
+        audio.write_audiofile(os.path.join(destination,name))
         print("finished file:", " index: ", index, " name:", name)
 
 def run():
@@ -37,15 +47,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-b","--base_path", type=str,
                         help="the base file path of the videos to convert")
-    parser.add_argument( "-f","--files", type=str,nargs='*', help="the names of the .mp4 files to convert")
+    parser.add_argument("-f","--files", type=str,nargs='*', help="the names of the .mp4 files to convert")
     parser.add_argument("-d","--destination",type=str, help="the destination folder to store the .mp3 files")
     parser.add_argument("-A","--ALL",action='store_true',help="if to convert all the .mp4 files under the path")
+    parser.add_argument("-M","--MultiplyVolume",type=int, default=1, help="multiply the volume of the mp3 file")
     args = parser.parse_args()
     
     if args.ALL:
         file_names = parse_file_names(absolute_path=args.base_path)
     else:
         file_names = args.files
-    convert(root=args.base_path,files=file_names,destination=args.destination)
+    convert(root=args.base_path,files=file_names,destination=args.destination,volume_multiply=args.MultiplyVolume)
         
     #run()
